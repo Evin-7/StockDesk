35734f595f9a2939198d7fcc24ac616d2d661752
evin leyander <evinleyander239@gmail.com>
2026-01-14 14:29:41 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -23,7 +23,8 @@ app.use("/api/items", itemRoutes);
 app.use("/api/customers", customerRoutes);
 app.use("/api/invoices", invoiceRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
